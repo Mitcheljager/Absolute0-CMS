a4a394e2020caef1d0d38ce78b4b71c6beb4ba66
Mitchel Jager <mitchel_jager@hotmail.com>
2016-12-22 01:47:47 +0100

    added view for pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,54 @@
 class PagesController < ApplicationController
   def index
-  end
-
-  def new
-  end
-
-  def edit
+    @pages = Page.all.order(created_at: :desc)
   end
 
   def show
+    @page = Page.find(params[:id])
+    @blocks = Block.where(page_id: params[:id]).all
+  end
+
+  def new
+    @page = Page.new
+  end
+
+  def edit
+    @page = Page.find(params[:id])
+  end
+
+  def update
+    @page = Page.find(params[:id])
+
+    if @page.update(page_params)
+      flash[:notice] = 'Page successfully updated'
+      render 'index'
+    else
+      render 'edit'
+    end
+  end
+
+  def create
+    @page = Page.new(page_params)
+
+    if @page.save
+      flash[:notice] = 'Page successfully created'
+      render 'index'
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @page = Page.find(params[:id])
+    @page.destroy
+
+    flash[:notice] = 'Page successfully deleted'
+    render 'index'
+  end
+
+  private
+
+  def page_params
+    params.require(:page).permit(:title)
   end
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,3 @@
 class Page < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 1 }
 end
