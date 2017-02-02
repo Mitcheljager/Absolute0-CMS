8c310fa34a8ed1ca55168e63ef55fe856ad4998b
Mitchel Jager <mitchel_jager@hotmail.com>
2017-02-02 17:23:29 +0100

    added alert on error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,7 @@ class SessionsController < ApplicationController
       redirect_to '/'
     else
     # If user's login doesn't work, send them back to the login form.
+      flash[:error] = 'Incorrect Username or Password'
       redirect_to '/login'
     end
   end
