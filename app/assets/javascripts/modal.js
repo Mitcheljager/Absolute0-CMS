9e05f1b8042866a95af185737afa97c13dbb3bb4
Mitcheljager <mitchel_jager@hotmail.com>
2017-05-02 11:50:19 +0200

    Revert "Revert "Merge branch 'master' into postgres-test""
    This reverts commit 38cb5e2d5d51bf3e7c1d140aa2f1ebb354b4648c.

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -13,13 +13,21 @@ Vue.component('Modal', {
 
 Vue.component('NewPageModal', {
   template: '#new-page-modal-template',
+  created: function() {
+    setTimeout(function(){
+      var positionLeft = $(".createPageModal")[0].getBoundingClientRect().left,
+          positionTop = $(".createPageModal")[0].getBoundingClientRect().top;
+
+      $(".modal__container").css({"left": positionLeft, "top": positionTop - 520});
+    });
+  },
   methods: {
     close: function() {
       app.showModal = false;
     },
     createPage: function() {
       app.showModal = false;
-      app.flashMessage = 'Page Created';
+      app.flashMessage = 'Page is successfully created';
 
       setTimeout(function() {
         $.getJSON('/pages/json/list.json', (json) => {
