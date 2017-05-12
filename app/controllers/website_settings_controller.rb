1d92f3693e3edf52f3b34447a6a432742743d58d
Mitcheljager <mitchel_jager@hotmail.com>
2017-05-12 12:13:09 +0200

    Changes to hopefully fix heroku

--- app/controllers/website_settings_controller.rb
+++ app/controllers/website_settings_controller.rb
@@ -1,18 +1,2 @@
 class WebsiteSettingsController < ApplicationController
-  def new
-    @website_setting = WebsiteSetting.new
-  end
-
-  def create
-    @website_setting = WebsiteSetting.new(website_setting_params)
-
-    if @website_setting.save
-      flash[:notice] = 'Website successfully saved'
-  end
-
-  private
-
-  def website_setting_params
-    params.require(:website_settings).permit(:homepage_id)
-  end
 end
