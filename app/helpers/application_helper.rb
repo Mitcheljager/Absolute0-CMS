7bb5703a87991c66dddbf1e425e26c335adc4d0f
Mitcheljager <mitchel_jager@hotmail.com>
2017-05-12 15:09:37 +0200

    Moved components list to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def componentsList(url = "app/views/components/items/admin")
+    a = Dir.entries(Rails.root.join(url))
+    a -= ['.', '..']
+    b = ["_",".html.erb"]
+    a.each do |p|
+      b.each {|x| p.slice!(x) }
+    end
+  end
 end
